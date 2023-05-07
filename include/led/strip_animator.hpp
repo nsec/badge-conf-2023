2612380723fb88e6927886088b4743b5eb39f1bb
Jérémie Galarneau <jeremie.galarneau@gmail.com>
2023-05-06 22:37:07 -0400

    LED strip animator: implement a keyframe based animation mechanism
    LED animations can now be defined as a series of keyframes, supporting
    offsets between leds, looping points, active/inactive LEDs.
    
    I am still working on storing the keyframes in flash rather than RAM,
    but the progress bar currently uses this new animation mechanism.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@gmail.com>

--- include/led/strip_animator.hpp
+++ include/led/strip_animator.hpp
@@ -27,7 +27,72 @@ public:
 	void setup() noexcept;
 
 	void set_current_animation_idle(unsigned int current_level) noexcept;
-	void set_breathing_led_count(unsigned int led_count) noexcept;
+	void set_red_to_green_led_progress_bar(uint8_t led_count) noexcept;
+
+	struct led_color {
+		led_color() = default;
+		led_color(uint8_t r_in, uint8_t g_in, uint8_t b_in)
+		{
+			components[np_red_offset] = r_in;
+			components[np_green_offset] = g_in;
+			components[np_blue_offset] = b_in;
+		}
+
+		explicit led_color(uint8_t *native_device_pixel_storage)
+		{
+			memcpy(components, native_device_pixel_storage, sizeof(components));
+		}
+
+		led_color(const led_color& in_color)
+		{
+			memcpy(components, in_color.components, sizeof(components));
+		}
+
+		uint8_t r() const noexcept
+		{
+			return components[np_red_offset];
+		}
+		uint8_t g() const noexcept
+		{
+			return components[np_green_offset];
+		}
+		uint8_t b() const noexcept
+		{
+			return components[np_blue_offset];
+		}
+
+		void log(const __FlashStringHelper *context) const
+		{
+			Serial.print(context);
+			Serial.print(F("{"));
+			Serial.print(int(r()));
+			Serial.print(F(", "));
+			Serial.print(int(g()));
+			Serial.print(F(", "));
+			Serial.print(int(b()));
+			Serial.println(F("}"));
+		}
+
+		uint8_t components[3];
+
+	private:
+		// Offsets in the three-byte group forming a pixel given our device's internal pixel format.
+		static const uint8_t np_red_offset = (NEO_GRB >> 4) & 0b11;
+		static const uint8_t np_green_offset = (NEO_GRB >> 2) & 0b11;
+		static const uint8_t np_blue_offset = NEO_GRB & 0b11;
+	};
+
+	struct keyframe {
+		keyframe() = default;
+		keyframe(const led_color& in_color, uint16_t at_time) :
+			color{ in_color }, time{ at_time }
+		{
+		}
+
+		led_color color;
+		// Time (in ms) since the animation started.
+		uint16_t time;
+	};
 
 protected:
 	void run(scheduling::absolute_time_ms current_time_ms) noexcept override;
@@ -35,24 +100,64 @@ protected:
 private:
 	enum class animation_type {
 		LEGACY,
+		KEYFRAMED,
+	};
+
+	enum class keyframed_animation : uint8_t {
+		PROGRESS_BAR,
 	};
 
 	void _legacy_animation_tick() noexcept;
+	void _keyframe_animation_tick(const scheduling::absolute_time_ms& current_time_ms) noexcept;
+	led_color _color(uint8_t led_id) const noexcept;
+	void _reset_keyframed_animation_state() noexcept;
 
 	Adafruit_NeoPixel _pixels;
-	animation_type _current_animation;
+	animation_type _current_animation_type;
+
+	// Keyframe indices of 4-bits each, use helpers to access.
+	struct indice_storage_element {
+		uint8_t even : 4;
+		uint8_t odd : 4;
+	};
 
 	union {
 		struct {
 			uint8_t level;
 		} legacy;
-	} _animation_config;
+		struct {
+			union {
+				struct {
+				} progress_bar;
+			};
+			uint8_t keyframe_count : 4;
+			uint8_t loop_point_index : 4;
+			keyframed_animation _animation;
+			const keyframe *keyframes;
+			// 1-bit per led. When inactive, the origin keyframe is repeated.
+			uint16_t active;
+		} keyframed;
+	} _config;
 	union {
 		struct {
 			uint8_t start_position;
-
 		} legacy;
-	} _animation_state;
+		struct {
+			union {
+				struct {
+				} progress_bar;
+			};
+
+			indice_storage_element _origin_keyframe_index[8];
+			indice_storage_element _destination_keyframe_index[8];
+			uint8_t ticks_since_start_of_animation[16];
+		} keyframed;
+	} _state;
+
+	uint8_t _get_keyframe_index(const indice_storage_element *indices, uint8_t led_id) const noexcept;
+	void _set_keyframe_index(indice_storage_element *indices, uint8_t led_id, uint8_t index) noexcept;
+
+
 };
 } // namespace nsec::led
 #endif // NSEC_LED_STRIP_ANIMATOR_HPP
--- src/strip_animator.cpp
+++ src/strip_animator.cpp
@@ -13,11 +13,50 @@ namespace ns = nsec::scheduling;
 namespace ng = nsec::g;
 
 namespace {
-constexpr ns::relative_time_ms initial_refresh_period_ms = 200;
+constexpr int16_t scaling_factor = 1024;
+
+// Linear interpolation in RGB space: not terrible, not great...
+nl::strip_animator::led_color interpolate(const nl::strip_animator::keyframe& origin,
+					  const nl::strip_animator::keyframe& destination,
+					  uint16_t current_time)
+{
+	nl::strip_animator::led_color new_color;
+
+	if (current_time >= destination.time) {
+		return destination.color;
+	}
+
+	const int32_t numerator = (int32_t(current_time) - int32_t(origin.time)) * scaling_factor;
+	const int32_t denominator =
+		(int32_t(destination.time) - int32_t(origin.time)) * scaling_factor;
+	for (uint8_t i = 0; i < sizeof(new_color.components); i++) {
+		const int32_t origin_component = origin.color.components[i];
+		const int32_t destination_component = destination.color.components[i];
+		const int32_t total_diff = destination_component - origin_component;
+		const int32_t diff_to_apply = ((total_diff * numerator) / denominator);
+
+		new_color.components[i] = uint8_t(origin_component + diff_to_apply);
+	}
+
+	return new_color;
 }
 
+const nl::strip_animator::keyframe red_to_green_progress_bar_keyframe_template[] = {
+	// off
+	{ { 0, 0, 0 }, 0 },
+	// red
+	{ { 25, 0, 0 }, 2000 },
+	// green <- beginning of loop
+	{ { 0, 100, 0 }, 4000 },
+	// dimmed green to green loop
+	{ { 0, 5, 0 }, 5000 },
+	{ { 0, 100, 0 }, 6000 },
+};
+
+} // namespace
+
 nl::strip_animator::strip_animator() noexcept :
-	ns::periodic_task(initial_refresh_period_ms),
+	ns::periodic_task(100) /* Set by the various animations. */,
 	_pixels(NUMPIXELS, P_NEOP, NEO_GRB + NEO_KHZ800)
 {
 	ng::the_scheduler.schedule_task(*this);
@@ -30,20 +69,22 @@ void nl::strip_animator::setup() noexcept
 
 void nl::strip_animator::_legacy_animation_tick() noexcept
 {
-	if (_animation_state.legacy.start_position > 15) {
+	if (_state.legacy.start_position > 15) {
 		// wrap around when reaching the end led strip
-		_animation_state.legacy.start_position = 0;
+		_state.legacy.start_position = 0;
 	}
 
 	// Set all pixel colors to 'off'
 	_pixels.clear();
-	for (unsigned int i = 0; i < _animation_config.legacy.level; i++) // determine how many LED should be ON
+	for (unsigned int i = 0; i < _config.legacy.level; i++) // determine how many LED
+								// should be ON
 	{
 		// led_ID is the current LED index that we are update.
-		uint8_t const led_ID = (_animation_state.legacy.start_position + i) % 16; // This is going over every
-								   // single LED that needs to
-								   // be on based on the
-								   // current LVL
+		uint8_t const led_ID = (_state.legacy.start_position + i) % 16; // This is going
+										// over every single
+										// LED that needs to
+										// be on based on
+										// the current LVL
 
 		// enable more colors if your lvl is more than the 16 LEDs
 		uint8_t b = 0;
@@ -113,29 +154,163 @@ void nl::strip_animator::_legacy_animation_tick() noexcept
 		_pixels.setPixelColor(led_ID, _pixels.Color(r, g, b));
 	}
 
-	_animation_state.legacy.start_position++;
-	// Send the updated pixel colors to the hardware.
-	_pixels.show();
+	_state.legacy.start_position++;
+}
+
+uint8_t nl::strip_animator::_get_keyframe_index(const indice_storage_element *indices,
+						uint8_t led_id) const noexcept
+{
+	const auto& element = indices[led_id / 2];
+
+	return led_id & 1 ? element.odd : element.even;
+}
+
+void nl::strip_animator::_set_keyframe_index(indice_storage_element *indices,
+					     uint8_t led_id,
+					     uint8_t new_keyframe_index) noexcept
+{
+	auto& element = indices[led_id / 2];
+
+	if (led_id & 1) {
+		element.odd = new_keyframe_index;
+	} else {
+		element.even = new_keyframe_index;
+	}
+}
+
+void nl::strip_animator::_keyframe_animation_tick(
+	const scheduling::absolute_time_ms& current_time_ms) noexcept
+{
+	// Animation specific setup
+	switch (_config.keyframed._animation) {
+	default:
+		break;
+	}
+
+	for (uint8_t i = 0; i < 16; i++) {
+		const auto origin_keyframe_index = _get_keyframe_index(_state.keyframed._origin_keyframe_index, i);
+		const auto destination_keyframe_index =
+			_get_keyframe_index(_state.keyframed._destination_keyframe_index, i);
+
+		const auto& origin_keyframe = _config.keyframed.keyframes[origin_keyframe_index];
+		const bool led_animation_is_active = (_config.keyframed.active >> i) & 1;
+
+		if (!led_animation_is_active) {
+			// Inactive, repeat the origin keyframe.
+			_pixels.setPixelColor(i,
+					      origin_keyframe.color.r(),
+					      origin_keyframe.color.g(),
+					      origin_keyframe.color.b());
+			continue;
+		}
+
+		//Serial.println();
+
+		const auto time_since_animation_start =
+			_state.keyframed.ticks_since_start_of_animation[i] * period_ms();
+		//Serial.print(F("Time since animation start: "));
+		//Serial.println(time_since_animation_start);
+
+		//origin_keyframe.color.log(F("origin keyframe: "));
+		if (_state.keyframed.ticks_since_start_of_animation[i] != 255) {
+			// Saturate counter.
+			_state.keyframed.ticks_since_start_of_animation[i]++;
+		}
+
+		// Interpolate to find the current color.
+		const auto& destination_keyframe =
+			_config.keyframed.keyframes[destination_keyframe_index];
+		//destination_keyframe.color.log(F("destination keyframe: "));
+
+		const auto new_color = interpolate(origin_keyframe,
+						   destination_keyframe,
+						   uint16_t(time_since_animation_start));
+		//new_color.log(F("interpolated color: "));
+		_pixels.setPixelColor(i, new_color.r(), new_color.g(), new_color.b());
+
+		// Advance keyframes if needed
+		if (time_since_animation_start < destination_keyframe.time) {
+			continue;
+		}
+
+		uint8_t new_origin_keyframe_index = destination_keyframe_index;
+		uint8_t new_destination_keyframe_index = destination_keyframe_index + 1;
+		if (new_destination_keyframe_index >= _config.keyframed.keyframe_count) {
+			// Loop back to the configured loop point. Move the current animation time in the past.
+			new_origin_keyframe_index = _config.keyframed.loop_point_index;
+			new_destination_keyframe_index = min(new_origin_keyframe_index + 1,
+							     _config.keyframed.keyframe_count - 1);
+			_state.keyframed.ticks_since_start_of_animation[i] =
+				_config.keyframed.keyframes[new_origin_keyframe_index].time /
+				period_ms();
+		} else {
+			new_origin_keyframe_index = destination_keyframe_index;
+			new_destination_keyframe_index = destination_keyframe_index + 1;
+		}
+
+		_set_keyframe_index(_state.keyframed._origin_keyframe_index, i, new_origin_keyframe_index);
+		_set_keyframe_index(_state.keyframed._destination_keyframe_index, i, new_destination_keyframe_index);
+	}
 }
 
 void nl::strip_animator::run(scheduling::absolute_time_ms current_time_ms) noexcept
 {
-	switch (_current_animation) {
+	switch (_current_animation_type) {
 	case animation_type::LEGACY:
 		_legacy_animation_tick();
+		break;
+	case animation_type::KEYFRAMED:
+		_keyframe_animation_tick(current_time_ms);
+		break;
 	default:
 		break;
 	}
+
+	// Send the updated pixel colors to the hardware.
+	_pixels.show();
+}
+
+nl::strip_animator::led_color nl::strip_animator::_color(uint8_t led_id) const noexcept
+{
+	return led_color(&_pixels.getPixels()[led_id * 3]);
 }
 
 void nl::strip_animator::set_current_animation_idle(unsigned int current_level) noexcept
 {
-	_current_animation = animation_type::LEGACY;
-	_animation_state.legacy.start_position = 0;
-	_animation_config.legacy.level = current_level;
+	period_ms(200);
+	_current_animation_type = animation_type::LEGACY;
+	_state.legacy.start_position = 0;
+	_config.legacy.level = current_level;
 }
 
-void nl::strip_animator::set_breathing_led_count(unsigned int current_level) noexcept
+void nl::strip_animator::_reset_keyframed_animation_state() noexcept
 {
-	set_current_animation_idle(current_level);
+	memset(&_state.keyframed, 0, sizeof(_state.keyframed));
+}
+
+void nl::strip_animator::set_red_to_green_led_progress_bar(uint8_t active_led_count) noexcept
+{
+	bool is_current_animation = _current_animation_type == animation_type::KEYFRAMED &&
+		_config.keyframed._animation == keyframed_animation::PROGRESS_BAR;
+
+	if (!is_current_animation) {
+		period_ms(30);
+
+		// Setup animation parameters.
+		_current_animation_type = animation_type::KEYFRAMED;
+		_config.keyframed._animation = keyframed_animation::PROGRESS_BAR;
+		_config.keyframed.active = 0;
+		_config.keyframed.keyframe_count =
+			sizeof(red_to_green_progress_bar_keyframe_template) /
+			sizeof(*red_to_green_progress_bar_keyframe_template);
+		_config.keyframed.keyframes = red_to_green_progress_bar_keyframe_template;
+		_config.keyframed.loop_point_index = 2;
+
+		// Clear its state.
+		_reset_keyframed_animation_state();
+	}
+
+	for (uint8_t i = 0; i < active_led_count; i++) {
+		_config.keyframed.active |= (1 << i);
+	}
 }
